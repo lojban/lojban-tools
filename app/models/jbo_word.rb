dec19f34feb24c2c98658dbdf87f9bdcb05c5ac8
Steve Sloan <steve@finagle.org>
2008-06-08 18:49:44 -0700

    JboWord: added to_xml (in jbovlaste format)

--- app/models/jbo_word.rb
+++ app/models/jbo_word.rb
@@ -6,4 +6,17 @@ class JboWord < ActiveRecord::Base
   has_many :parts, :class_name => 'JboPart'
   has_many :eng_words
 
+  def to_xml( options = {} )
+    options[:indent] ||= 2
+    xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
+    xml.instruct! unless options[:skip_instruct]
+
+    xml.valsi :word => name, :type => jbo_type.name do
+      xml.selmaho jbo.token.name  if jbo_token
+      xml.definition defn  if defn
+      xml.notes notes  if notes
+      parts.each { |p|  xml.rafsi p.name }
+    end
+  end
+
 end
